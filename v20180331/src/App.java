856f8a268807086d9f4fee4bb2d3aed535a59e3c
李汉朋 <lihp@oupuzw.com>
2018-03-31 16:39:26 +0800

    练习

---
+++ v20180331/src/App.java
@@ -0,0 +1,27 @@
+public class App {
+
+    public static void main(String[] asff) {
+
+//        Lhp lhp = new Lhp();
+//        Lhp lhp2 = new Lhp();
+//
+//        lhp.print();
+//        lhp.result();
+//
+//
+//        lhp.add(1, 1.1);
+//
+//        // 字符串
+//        String str = "";
+//
+//        // 数字
+//        Integer ii = 1;
+//        Double f = 0.1;
+//
+//        // bool类型 true，false
+//        boolean t = true;
+
+
+        System.out.println("Hello World!");
+    }
+}
---
+++ v20180331/src/Lhp.java
@@ -0,0 +1,19 @@
+/**
+ * Created by lhp on 2018/3/31.
+ */
+public class Lhp {
+
+    public void print() {
+
+    }
+
+    public String result() {
+        return "lhp";
+    }
+
+    public Integer add(Integer aa, Double b) {
+        return aa + b.intValue();
+//        return 0;
+    }
+
+}
