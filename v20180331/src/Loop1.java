a97794017f9c17af39cf6c4d1bff3ccf122ff09b
lsklkgold <37950893+lsklkgold@users.noreply.github.com>
2018-04-15 10:40:35 +0800

    1111

---
+++ v20180331/src/Loop1.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2018/4/15.
+ */
+public class Loop1 {
+    public static void main(String args[] ){
+        int y=0;
+        for(int x=1;x<=10000;x=x+1){
+            y=y+x;
+            System.out.println((y));
+        }
+
+    }
+}
