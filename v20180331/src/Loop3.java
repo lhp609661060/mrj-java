7eb4b09f127619b5a09a105b75491d1a33793f6f
lsklkgold <37950893+lsklkgold@users.noreply.github.com>
2018-04-15 10:48:41 +0800

    1111

---
+++ v20180331/src/Loop3.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2018/4/15.
+ */
+public class Loop3
+{
+    public static void main(String args[] ){
+        int y=0;
+        for(int x=1;x<=10001;x=x+2){
+            y=y+x;
+            System.out.println((y));
+        }
+
+    }
+}
+
